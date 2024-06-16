12d0531e345751af00b632a23524942947f809d3
Rifat Waheed <rifatwaheed@gmail.com>
2024-06-17 01:12:50 +0600

    image loaded

--- ThriftShopAngular/src/app/app.component.ts
+++ ThriftShopAngular/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './view/navbar/navbar.component';
+import { HomeComponent } from "./view/home/home.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [RouterOutlet, NavbarComponent]
+    imports: [RouterOutlet, NavbarComponent, HomeComponent]
 })
 export class AppComponent {
   title = 'ThriftShopAngular';
