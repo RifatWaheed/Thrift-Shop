1cab0ddaf338c5e0004c72c557b7a98ec204cfa6
Rifat Waheed <rifatwaheed@gmail.com>
2024-07-07 21:01:08 +0600

    primeng table created

--- ThriftShopAngular/src/app/view/admin-panel/products/products.component.ts
+++ ThriftShopAngular/src/app/view/admin-panel/products/products.component.ts
@@ -3,11 +3,12 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { TitleComponent } from '../../title/title.component';
 import { TableModule } from 'primeng/table';
+import { ImportsModule } from '../../../../import/imports.module';
 
 @Component({
   selector: 'products',
   standalone: true,
-  imports: [TitleComponent,TableModule ],
+  imports: [TitleComponent,TableModule],
   templateUrl: './products.component.html',
   styleUrl: './products.component.scss'
 })
@@ -23,6 +24,26 @@ export class ProductsComponent {
     category: 'Accessories',
     quantity: 24,
     inventoryStatus: 'INSTOCK',
-    rating: 5}
+    rating: 5},
+    { id: '1000',
+      code: 'f230fh0g3',
+      name: 'Bamboo Watch',
+      description: 'Product Description',
+      image: 'bamboo-watch.jpg',
+      price: 65,
+      category: 'Accessories',
+      quantity: 24,
+      inventoryStatus: 'INSTOCK',
+      rating: 5},
+      { id: '1000',
+        code: 'f230fh0g3',
+        name: 'Bamboo Watch',
+        description: 'Product Description',
+        image: 'bamboo-watch.jpg',
+        price: 65,
+        category: 'Accessories',
+        quantity: 24,
+        inventoryStatus: 'INSTOCK',
+        rating: 5}
    ]
 }
