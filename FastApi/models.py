436d6e0a30b5ab2dceda348914e9fed0ffef36bc
Rifat Waheed <rifatwaheed@gmail.com>
2024-07-07 23:25:28 +0600

    fastapi deleted

--- FastApi/models.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import Column,Integer
-from .database import Base
-
-class Products(Base):
-    __tablename__ = "products"
-
-    pk_id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
-    
