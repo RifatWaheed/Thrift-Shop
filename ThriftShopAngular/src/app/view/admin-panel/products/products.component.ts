b0f6e52ca2a287f01f7329d2c68d1a37fa310fb7
Rifat Waheed <rifatwaheed@gmail.com>
2024-07-07 13:11:47 +0600

    primeng dummy table created

--- ThriftShopAngular/src/app/view/admin-panel/products/products.component.ts
+++ ThriftShopAngular/src/app/view/admin-panel/products/products.component.ts
@@ -2,14 +2,27 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { TitleComponent } from '../../title/title.component';
+import { TableModule } from 'primeng/table';
 
 @Component({
   selector: 'products',
   standalone: true,
-  imports: [TitleComponent],
+  imports: [TitleComponent,TableModule ],
   templateUrl: './products.component.html',
   styleUrl: './products.component.scss'
 })
 export class ProductsComponent {
 
+  products : any = [
+   { id: '1000',
+    code: 'f230fh0g3',
+    name: 'Bamboo Watch',
+    description: 'Product Description',
+    image: 'bamboo-watch.jpg',
+    price: 65,
+    category: 'Accessories',
+    quantity: 24,
+    inventoryStatus: 'INSTOCK',
+    rating: 5}
+   ]
 }
