a2d336527cfeb5f97507ee4936ef68dd0c566899
Rifat Waheed <rifatwaheed@gmail.com>
2024-07-08 22:58:36 +0600

    deleted files again

--- FastApiProject/app/ContactInfo/models.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String
-from app.database import Base
-
-class ContactInfo(Base):
-    __tablename__ = "contactInfo"
-    pk_id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, nullable=False)
-    phone = Column(String, nullable=True)
-    email = Column(String, nullable=True)
