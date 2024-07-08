63d0c7787e7d241c18f71ea5380c7a5950938dbf
Rifat Waheed <rifatwaheed@gmail.com>
2024-07-08 22:07:09 +0600

    everything running smoothly till now

--- FastApiProject/app/main.py
+++ FastApiProject/app/main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
 from app.database import engine
+from app.ContactInfo import api as contactInfoApi
+from app.ContactInfo import models as contactInfoModels
 
 
 app = FastAPI()
+
+app.include_router(contactInfoApi.router, prefix="/api", tags=["contactInfo"])
 
 @app.get("/")
 async def root():
