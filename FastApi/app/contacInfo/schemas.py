9cf3fba7aa62fb56830f34547836a33b9a601739
Rifat Waheed <rifatwaheed@gmail.com>
2024-07-09 02:30:39 +0600

    Revert "pkID spelling fixed"
    This reverts commit e1ba6f15bdd805bee58861306f4407ff4b70d30a.

--- FastApi/app/contacInfo/schemas.py
+++ FastApi/app/contacInfo/schemas.py
@@ -2,16 +2,15 @@ from pydantic import BaseModel
 from typing import Optional
 
 class ContactInfoBase(BaseModel):
+    pkID: int
     name: str
-    phone: Optional[str] = None
-    email: Optional[str] = None
 
 class ContactInfoCreate(ContactInfoBase):
     pkID: int  # Assuming this field is required for the search operation
 
 class ContactInfo(ContactInfoBase):
-    pkID: int
-
+    phone: Optional[str] = None
+    email: Optional[str] = None
 
     class Config:
         from_attributes = True
