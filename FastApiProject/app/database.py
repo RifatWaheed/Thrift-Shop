24fea3014f5c53dd12c8851e495a46a1a87ca7e8
Rifat Waheed <rifatwaheed@gmail.com>
2024-07-08 21:47:10 +0600

    database.py code edited for connection

---
+++ FastApiProject/app/database.py
@@ -0,0 +1,16 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Get the database URL from environment variables
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+# Create the SQLAlchemy engine for the PostgreSQL database
+engine = create_engine(DATABASE_URL)
+
+# Create a configured "Session" class
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
