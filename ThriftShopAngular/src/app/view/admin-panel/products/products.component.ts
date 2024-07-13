b88388debbd8aed26a73e05b6c5b668b9b23a4f4
Rifat Waheed <rifatwaheed@gmail.com>
2024-07-13 20:49:02 +0600

    primeng dropdown created

--- ThriftShopAngular/src/app/view/admin-panel/products/products.component.ts
+++ ThriftShopAngular/src/app/view/admin-panel/products/products.component.ts
@@ -1,31 +1,44 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { TitleComponent } from '../../title/title.component';
 import { TableModule } from 'primeng/table';
 import { ImportsModule } from '../../../../import/imports.module';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { FormsModule } from '@angular/forms';
+import { DropdownModule } from 'primeng/dropdown';
+
+interface City {
+  name: string;
+  code: string;
+}
 
 @Component({
   selector: 'products',
   standalone: true,
-  imports: [TitleComponent,TableModule],
+  imports: [TitleComponent, TableModule, CommonModule, RouterOutlet, RouterLink, RouterLinkActive, ButtonModule, InputTextModule, FloatLabelModule, FormsModule, DropdownModule],
   templateUrl: './products.component.html',
   styleUrl: './products.component.scss'
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
 
-  products : any = [
-   { id: '1000',
-    code: 'f230fh0g3',
-    name: 'Bamboo Watch',
-    description: 'Product Description',
-    image: 'bamboo-watch.jpg',
-    price: 65,
-    category: 'Accessories',
-    quantity: 24,
-    inventoryStatus: 'INSTOCK',
-    rating: 5},
-    { id: '1000',
+
+
+  value
+
+  selectedProduct
+
+
+  cities: City[] | undefined;
+
+  selectedCity: City | undefined;
+
+
+  products: any = [
+    {
+      id: '1000',
       code: 'f230fh0g3',
       name: 'Bamboo Watch',
       description: 'Product Description',
@@ -34,16 +47,42 @@ export class ProductsComponent {
       category: 'Accessories',
       quantity: 24,
       inventoryStatus: 'INSTOCK',
-      rating: 5},
-      { id: '1000',
-        code: 'f230fh0g3',
-        name: 'Bamboo Watch',
-        description: 'Product Description',
-        image: 'bamboo-watch.jpg',
-        price: 65,
-        category: 'Accessories',
-        quantity: 24,
-        inventoryStatus: 'INSTOCK',
-        rating: 5}
-   ]
+      rating: 5
+    },
+    {
+      id: '1000',
+      code: 'f230fh0g3',
+      name: 'Bamboo Watch abcdrefefefefef',
+      description: 'Product Description',
+      image: 'bamboo-watch.jpg',
+      price: 65,
+      category: 'Accessories',
+      quantity: 24,
+      inventoryStatus: 'INSTOCK',
+      rating: 5
+    },
+    {
+      id: '1000',
+      code: 'f230fh0g3',
+      name: 'Bamboo Watch',
+      description: 'Product Description',
+      image: 'bamboo-watch.jpg',
+      price: 65,
+      category: 'Accessories',
+      quantity: 24,
+      inventoryStatus: 'INSTOCK',
+      rating: 5
+    }
+  ]
+
+
+  ngOnInit(): void {
+    this.cities = [
+      { name: 'New York', code: 'NY' },
+      { name: 'Rome', code: 'RM' },
+      { name: 'London', code: 'LDN' },
+      { name: 'Istanbul', code: 'IST' },
+      { name: 'Paris', code: 'PRS' }
+    ];
+  }
 }
